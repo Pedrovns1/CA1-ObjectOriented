18ccf3a8dc6f5256a9124412bc0888fd1f7c9fcf
Pedro Vinicius <peuvini.94@gmail.com>
2024-01-15 17:45:15 +0000

    Initial commit

---
+++ src/ca1/objectoriented/CA1ObjectOriented.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ca1.objectoriented;
+
+/**
+ *
+ * @author peuvi
+ */
+public class CA1ObjectOriented {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
