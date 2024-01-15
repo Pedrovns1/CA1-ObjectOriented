5b2549a7caf2c260aab7c723b5d0ffaf85242215
Pedro Vinicius <peuvini.94@gmail.com>
2024-01-15 18:30:10 +0000

    Scanner added
    A scanner has been added to the code to be able to read the file students.txt."

--- src/ca1/objectoriented/CA1ObjectOriented.java
+++ src/ca1/objectoriented/CA1ObjectOriented.java
@@ -4,6 +4,9 @@
  */
 package ca1.objectoriented;
 
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author peuvi
@@ -14,7 +17,18 @@ public class CA1ObjectOriented {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        try {
+            Scanner sc = new Scanner(new FileReader("C:\\Users\\peuvi\\Documents\\NetBeansProjects\\CA1-ObjectOriented\\students.txt"));
+
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+
+        } catch (Exception e) {
+            System.out.println("e");
+        }
+
     }
-    
+
 }
