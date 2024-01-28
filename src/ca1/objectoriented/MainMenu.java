a60dd78edc01e8a61ab0ce982b356bf20d008fde
Pedro Vinicius <peuvini.94@gmail.com>
2024-01-28 17:57:24 +0000

    Methods addition
    I created a method getInput to get the user data.
    I also created a method to read from the file that usas the validations I first created on my main CA1ObjectOriented.java.
    The addDataViaConsole will get the information from my getInput and also use the validations from my main.

--- src/ca1/objectoriented/MainMenu.java
+++ src/ca1/objectoriented/MainMenu.java
@@ -5,7 +5,9 @@
 package ca1.objectoriented;
 
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -13,19 +15,14 @@ import java.util.Scanner;
  * @author peuvi
  */
 public class MainMenu {
-    
+
     public void showMenu() {
-    
-        Scanner sc = new Scanner (System.in);
+
+        Scanner sc = new Scanner(System.in);
         BufferedWriter writer = new BufferedWriter(new FileWriter("C:\\Users\\peuvi\\Documents\\NetBeansProjects\\CA1-ObjectOriented\\status.txt", true));
-    
- }
-    
-    
-    
-    
-    
-    
+
+    }
+
     private int getOption(Scanner sc) {
         while (true) {
             try {
@@ -35,6 +32,66 @@ public class MainMenu {
             }
         }
     }
-    
-       
+
+    private void readDataFromFile(BufferedWriter writer) {
+        try {
+            Scanner sc = new Scanner(new FileReader("C:\\Users\\peuvi\\Documents\\NetBeansProjects\\CA1-ObjectOriented\\students.txt"));
+
+            while (sc.hasNextLine()) {
+                String fullNameLine = sc.nextLine();
+
+                if (fullNameLine.isEmpty()) {
+                    sc.nextLine();
+                    sc.nextLine();
+                    continue;
+                }
+
+                String numClassesLine = sc.nextLine();
+                String studentIdLine = sc.nextLine();
+
+                if (!CA1ObjectOriented.isValidName(fullNameLine) || !CA1ObjectOriented.isValidnumClasses(numClassesLine) || !CA1ObjectOriented.isValidStudentID(studentIdLine)) {
+                    System.out.println("Invalid data in the file. Skipping.");
+                    continue;
+                }
+
+                int numClasses = Integer.parseInt(numClassesLine);
+                String studentNumber = studentIdLine;
+                String[] names = fullNameLine.split(" ");
+
+                CA1ObjectOriented.studentDataOutput(writer, studentNumber, names[1], numClasses);
+            }
+
+            sc.close();
+        } catch (IOException e) {
+            System.out.println("Error reading data from file: " + e.getMessage());
+        }
+    }
+
+    private String getInput(String prompt, Scanner sc) {
+        System.out.println(prompt);
+        return sc.nextLine();
+    }
+
+    private void addDataViaConsole(Scanner sc, BufferedWriter writer) throws IOException {
+
+        while (true) {
+            String fullNameLine = getInput("Enter the first and last name separated by one single space. (or type exit to stop):", sc);
+            String numClassesLine = getInput("Enter number of classes:", sc);
+            String studentIdLine = getInput("Enter student ID:", sc);
+
+            if (CA1ObjectOriented.isValidName(fullNameLine) && CA1ObjectOriented.isValidnumClasses(numClassesLine) && CA1ObjectOriented.isValidStudentID(studentIdLine)) {
+
+                int numClasses = Integer.parseInt(numClassesLine);
+                String studentNumber = studentIdLine;
+                String[] names = fullNameLine.split(" ");
+
+                CA1ObjectOriented.studentDataOutput(writer, studentNumber, names[1], numClasses);
+            } else {
+                System.out.println("Student data invalid. Please try again.");
+            }
+        }
+    }
+
 }
+
+
